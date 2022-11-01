45bc4f7175bef09f3d3ae3b63735d8e14265540f
cyanfhine <yifan03.chen@tum.de>
2022-11-01 13:34:15 +0100

    backup 1.2 so far everything is working

--- src/pgdp/math/PinguSqrt.java
+++ src/pgdp/math/PinguSqrt.java
@@ -15,6 +15,7 @@ public class PinguSqrt {
 		int start_subtrahend = 0;
 		int round = 0;
 		int number_d_pack = 0;
+		double print_result = 0;
 		String result = "";
 		String fixed_number = String.format("%1.4f", n);
 		String number = String.format("%1.4f", n);
@@ -23,7 +24,7 @@ public class PinguSqrt {
 		if (n < 0) {
 			System.out.println("Keine negativen Wurzeln!");
 		} else {
-			System.out.println("Wurzel aus " + fixed_number + "\n");
+			System.out.println("Wurzel aus " + String.valueOf(n) + "\n");
 			c_index = fixed_number.indexOf(".");
 			if (fixed_number.substring(0, c_index).length() % 2 != 0) {
 				number = "0" + number;
@@ -77,29 +78,28 @@ public class PinguSqrt {
 				d_packs_l = c_index / 2;
 			}
 			for (int r = 0; r < d_packs_l - 1; r++) {
-				minuend = Integer.parseInt(d_result + d_packs[number_d_pack]);
-				d_minuend = minuend;
-				start_subtrahend = Integer.parseInt(String.valueOf(Integer.parseInt(result) * 2) + "1");
-				subtrahend = 0;
-				d_result = minuend;
-				round = 0;
+					minuend = Integer.parseInt(d_result + d_packs[number_d_pack]);
+					d_minuend = minuend;
+					start_subtrahend = Integer.parseInt(String.valueOf(Integer.parseInt(result) * 2) + "1");
+					subtrahend = 0;
+					d_result = minuend;
+					round = 0;
 
-				print_BlockTop(minuend);
+					print_BlockTop(minuend);
 
-				while (d_result >= 0) {
-					subtrahend = subtrahend + start_subtrahend + 2 * round;
-					d_result = d_minuend - subtrahend;
-					round++;
-				}
-				round = round - 1;
-				subtrahend_printer(start_subtrahend, round);
-				subtrahend = subtrahend - (start_subtrahend + 2 * (round));
-				d_result = minuend - subtrahend;
-				result = result + String.valueOf(round);
-				number_d_pack = number_d_pack + 1;
-				print_BlockBottom(d_result, round);
+					while (d_result >= 0) {
+						subtrahend = subtrahend + start_subtrahend + 2 * round;
+						d_result = d_minuend - subtrahend;
+						round++;
+					}
+					round = round - 1;
+					subtrahend_printer(start_subtrahend, round);
+					subtrahend = subtrahend - (start_subtrahend + 2 * (round));
+					d_result = minuend - subtrahend;
+					result = result + String.valueOf(round);
+					number_d_pack = number_d_pack + 1;
+					print_BlockBottom(d_result, round);
 			}
-
 			//after comma
 
 			if ((fixed_number.length() - c_index - 1) % 2 != 0) {
@@ -109,52 +109,56 @@ public class PinguSqrt {
 			}
 			//System.out.println(d_packs_r);
 			for (int r = 0; r < d_packs_r; r++) {
-				minuend = Integer.parseInt(String.valueOf(d_result) + d_packs[number_d_pack]);
-				d_minuend = minuend;
-				start_subtrahend = Integer.parseInt(String.valueOf(Integer.parseInt(result) * 2) + "1");
-				subtrahend = 0;
-				d_result = minuend;
-				round = 0;
-				print_BlockTop(minuend);
+				if (result.substring(d_packs_l, result.length()).length() < 2) {
+					minuend = Integer.parseInt(String.valueOf(d_result) + d_packs[number_d_pack]);
+					d_minuend = minuend;
+					start_subtrahend = Integer.parseInt(String.valueOf(Integer.parseInt(result) * 2) + "1");
+					subtrahend = 0;
+					d_result = minuend;
+					round = 0;
+					print_BlockTop(minuend);
 
-				while (d_result >= 0) {
-					subtrahend = subtrahend + start_subtrahend + 2 * round;
-					d_result = d_minuend - subtrahend;
-					round++;
+					while (d_result >= 0) {
+						subtrahend = subtrahend + start_subtrahend + 2 * round;
+						d_result = d_minuend - subtrahend;
+						round++;
+					}
+					round = round - 1;
+					subtrahend_printer(start_subtrahend, round);
+					subtrahend = subtrahend - (start_subtrahend + 2 * (round));
+					d_result = minuend - subtrahend;
+					result = result + String.valueOf(round);
+					number_d_pack = number_d_pack + 1;
+					print_BlockBottom(d_result, round);
 				}
-				round = round - 1;
-				subtrahend_printer(start_subtrahend, round);
-				subtrahend = subtrahend - (start_subtrahend + 2 * (round));
-				d_result = minuend - subtrahend;
-				result = result + String.valueOf(round);
-				number_d_pack = number_d_pack + 1;
-				print_BlockBottom(d_result, round);
-
-				//result = result.substring(0, d_packs_l) + "." + result.substring(d_packs_l + 1, result.length());
-
 			}
 			//add 00 if there is still a rest
-			if (d_result != 0) {
-				minuend = Integer.parseInt(String.valueOf(d_result) + "00");
-				d_minuend = minuend;
-				start_subtrahend = Integer.parseInt(String.valueOf(Integer.parseInt(result) * 2) + "1");
-				subtrahend = 0;
-				d_result = minuend;
-				round = 0;
+			if (result.substring(d_packs_l, result.length()).length() < 2) {
+				if (d_result != 0) {
+					minuend = Integer.parseInt(String.valueOf(d_result) + "00");
+					d_minuend = minuend;
+					start_subtrahend = Integer.parseInt(String.valueOf(Integer.parseInt(result) * 2) + "1");
+					subtrahend = 0;
+					d_result = minuend;
+					round = 0;
 
-				while (d_result >= 0) {
-					subtrahend = subtrahend + start_subtrahend + 2 * round;
-					d_result = d_minuend - subtrahend;
-					round++;
+					while (d_result >= 0) {
+						subtrahend = subtrahend + start_subtrahend + 2 * round;
+						d_result = d_minuend - subtrahend;
+						round++;
+					}
+					round = round - 1;
+					subtrahend = subtrahend - (start_subtrahend + 2 * (round));
+					d_result = minuend - subtrahend;
+					result = result + String.valueOf(round);
+					number_d_pack = number_d_pack + 1;
 				}
-				round = round - 1;
-				subtrahend = subtrahend - (start_subtrahend + 2 * (round));
-				d_result = minuend - subtrahend;
-				result = result + String.valueOf(round);
-				number_d_pack = number_d_pack + 1;
 			}
 
-			System.out.println("Ergebnis: " + result.substring(0, d_packs_l) + "." + result.substring(d_packs_l, result.length()));
+
+			print_result = Double.parseDouble(result.substring(0, d_packs_l) + "." + result.substring(d_packs_l, result.length()));
+			result = String.valueOf(print_result);
+			System.out.println("Ergebnis: " + result);
 		}
 	}
 
@@ -180,20 +184,19 @@ public class PinguSqrt {
 		//sqrt(101);
 		//sqrt(1049.76);
 
-		sqrt(2147483647); //funktioniert nicht! weil Java String format
+		//sqrt(2147483647); //(erledigt) funktioniert nicht! weil Java String format
 
 		//sqrt(101.231);
 		//sqrt(-1);
 
-		//sqrt(0); idk ob das so passt
+		//sqrt(0); //idk ob das so passt
 
 		//sqrt(4);
 
-		//sqrt(0.00123); nur bis 4. Stelle, aber volle Zahl ausgeben
+		//sqrt(0.00123); //(erledigt) nur bis 4. Stelle, aber volle Zahl ausgeben
 
-		//sqrt(0.0004);
+		//sqrt(0.0004); //(erledigt) Ausgabe nur bis 2. Nachkommastelle
 
-		//Ausgabe nur bis 2. Nachkommastelle
 	}
 
 }
